d39aae8dcd3c5a965ed42e12ea09bd63a4ecfd54
RyuGeunHwan <h46344@naver.com>
2022-08-12 15:47:43 +0900

    feat : history업로드

---
+++ .history/vue-emit/src/views/About_20220812143733.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>이곳은 어바웃 페이지입니다.</h2>
+    <KossieCoder title="About title" name="류근환" />
+  </div>
+</template>
+
+<script>
+import KossieCoder from '@/components/KossieCoder.vue';
+ 
+export default {
+  components:{
+    KossieCoder
+  }
+}
+</script>
+
+<style scoped>
+h2{
+  color : red
+}
+</style>
---
+++ .history/vue-emit/src/views/Home_20220812143733.vue
@@ -0,0 +1,42 @@
+<!-- template태그 : HTML이 들어가는 구역  -->
+<template>
+  <div>
+    <h1>This is Home Page</h1>
+    <h2>이곳은 홈 페이지 입니다.</h2>
+    <form action="">
+      <InputField v-model="name"/>
+        <button>Submit</button>
+    </form>
+    {{name}}
+  </div>
+</template>
+
+<!-- script태그 : 옵션,데이터,computed,watch 등이 들어가는 구역 -->
+<script>
+import InputField from '@/components/InputField.vue'
+
+export default {
+  components:{
+    InputField
+  },
+  data(){
+    return{
+      name : ''
+    }
+  },
+  // methods:{
+  //   updateName(name){
+  //     this.name=name;
+  //   }
+  // }
+}
+</script>
+
+
+<!-- style : CSS코드 -->
+<!-- scoped를 넣어줘야 위에보이는 template의 html태그에 적용이 된다. scoped를 않넣으면 vue어플리케이션 전체에 적용이 된다.-->
+<style scoped>
+h1{
+  color : red
+}
+</style>
